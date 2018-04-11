612b802986f647cdeccfd630765e9c700c60b0b8
Maciej Bartoszuk <bartoszuk@rexamine.com>
2018-04-11 16:20:15 +0200

    added GPLv3 licence to source files

--- src/pdgmykernelcomparator.cpp
+++ src/pdgmykernelcomparator.cpp
@@ -1,11 +1,19 @@
-
-/*
- * pdgmykernelcomparator.cpp
+/*    SimilaR package for R
+ *    Copyright (C) 2018 M. Bartoszuk, M. Gagolewski
  *
- *  Created on: 09 aug 2016
- *  Refactored: 28 sep 2016
- *      Author: bartoszukm
- */
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */   
 
 #include "pdgmykernelcomparator.h"
 
